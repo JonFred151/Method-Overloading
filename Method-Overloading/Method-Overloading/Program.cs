563d892f7f4191bf8f3c26ffb643c96b73957828
Jon Frederick <jonFred151@gmail.com>
2023-11-29 13:49:55 -0600

    Finish the Methods

--- Method-Overloading/Method-Overloading/Program.cs
+++ Method-Overloading/Method-Overloading/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿namespace MethodOverloading
+{
+    class Program
+    {
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static double Add(double x, double y)
+        {
+            return (double)x + y;
+        }
+        public static string Add(double x, double y,  bool isTrue)
+        {
+            if (isTrue == true)
+            {
+                Add(x, y);
+                return $"{x + y} dollars";
+            }
+            else
+
+                return $"This is fake money!";
+        }
+        static void Main(string[] args)
+        {
+            var x = 2;
+            var y = 1;
+            var a = 1.04;
+            var b = 2.5;
+            var truth = true; 
+
+            Console.WriteLine(Add(x, y));
+            Console.WriteLine(Add(a, b));
+            Console.WriteLine(Add(a, x, truth));
+
+        }
+    }
+}
--- Method-Overloading/Method-Overloading/obj/Debug/net8.0/Method-Overloading.AssemblyInfo.cs
+++ Method-Overloading/Method-Overloading/obj/Debug/net8.0/Method-Overloading.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Method-Overloading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d577d90bc8375c774e190dd76e95856741afe65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Method-Overloading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Method-Overloading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
